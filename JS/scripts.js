2be423ba6b286b33812295d1a5a466aedb20a0ac
MorshedML <mdmorshed686374@gmail.com>
2025-09-07 16:29:19 +0600

    Categories nav data are dynamically loaded

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,5 +1,5 @@
 const categoryList = document.getElementById('category-list');
-const categoryListForMobile = document.getElementById('category-listForMobile');
+// const categoryListForMobile = document.getElementById('category-listForMobile');
 
 const loadCategory = () =>{
     const url = `https://openapi.programming-hero.com/api/categories`;
@@ -13,21 +13,34 @@ const loadCategory = () =>{
 
 const loadCategoryDataShow = categories =>{
     categoryList.innerHTML = '';
-    categoryListForMobile.innerHTML = '';
 
     categories.forEach(category =>{
         
         categoryList.innerHTML += `
-            <li class="hover:bg-[#15803D] hover:text-white p-2 rounded-lg  font-medium">${category.category_name}</li>
+            <li id="${category.id}" class="hover:bg-[#15803D] hover:text-white p-2 rounded-lg  font-medium">${category.category_name}</li>
         
         `
     })
 
-    categories.forEach(category =>{
-        categoryListForMobile.innerHTML += `
-            <button class="btn btn-outline btn-success font-semibold lesson-btn">${category.category_name}</button>
-        `
+
+    categoryList.addEventListener('click',e =>{
+        
+        const allLi = document.querySelectorAll('#category-list li');
+        
+        allLi.forEach(li =>{
+            li.classList.remove('bg-[#15803D]');
+            li.classList.remove('text-white');
+        })
+
+        if(e.target.localName === 'li'){
+            e.target.classList.add('bg-[#15803D]');
+            e.target.classList.add('text-white');
+            console.log(e.target.id);
+        }
+        
     })
+    
+
     
 }
 
